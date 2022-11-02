05d8367cd49b0964e5966a62f34fcdd72ce24a03
ILIA <ischenko.illa@lll.kpi.ua>
2022-11-02 09:59:18 +0200

    ...

---
+++ Task#1.py
@@ -0,0 +1,43 @@
+class Ticket:
+
+    def __init__(self, number, price=2):
+        self.number = number
+        self.price = price
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if not isinstance(value, int | float):
+            raise TypeError()
+        if value < 0.0:
+            raise ValueError()
+        self._price = value
+
+    def __str__(self):
+        return f'{self.number} - {self.price}'
+
+
+class Advance_Ticket:
+
+    def __init__(self, number):
+        super().__init__(number)
+        self.price = self.price - self.price * 0.4
+
+
+class Student_Ticket:
+
+    def __init__(self, number):
+        super().__init__(number)
+        self.price = self.price - self.price * 0.5
+
+
+class Late_Ticket:
+
+    def __init__(self, number):
+        super().__init__(number)
+        self.price = self.price + self.price * 0.1
+
+
